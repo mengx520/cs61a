c835bc94878997c043031975cab8d5a420476fe7
Alice Meng <mengx520@gmail.com>
2023-11-16 14:53:07 -0800

    Add hw01

--- hw01/hw01.py
+++ hw01/hw01.py
@@ -13,9 +13,9 @@ def a_plus_abs_b(a, b):
     ['return f(a, b)']
     """
     if b < 0:
-        f = _____
+        f = sub
     else:
-        f = _____
+        f = add
     return f(a, b)
 
 
@@ -37,7 +37,7 @@ def two_of_three(x, y, z):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(two_of_three)).body[0].body]
     ['Expr', 'Return']
     """
-    return _____
+    return x * x + y * y + z * z - max(x, y, z) * max(x, y, z)
 
 
 def largest_factor(n):
@@ -51,6 +51,9 @@ def largest_factor(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    for i in range(n//2, 0, -1):
+        if n % i == 0:
+            return i
 
 
 def if_function(condition, true_result, false_result):
@@ -96,12 +99,16 @@ def with_if_function():
 
 def cond():
     "*** YOUR CODE HERE ***"
+    return False
 
 def true_func():
     "*** YOUR CODE HERE ***"
+    print(42)
+
 
 def false_func():
     "*** YOUR CODE HERE ***"
+    print(47)
 
 
 def hailstone(n):
@@ -120,4 +127,14 @@ def hailstone(n):
     7
     """
     "*** YOUR CODE HERE ***"
-
+    step = 1
+    while n != 1:
+        print(n)
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = n * 3 + 1
+        step += 1
+    print(n) 
+    return step
+    
